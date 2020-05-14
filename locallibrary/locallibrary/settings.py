f69a6d52c3f78a5b441f7a2525cf6cd103bfa1c0
srinivas23 <srinivaschat23@gmail.com>
2020-05-14 11:58:14 +0530

    update settings,py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 }
 
 # for dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
